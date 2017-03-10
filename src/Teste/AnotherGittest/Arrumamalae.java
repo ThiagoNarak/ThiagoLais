fe55ba1167e59ca5b723c872ce9d48c75b4373b4
Thiago rodrigo <thiago.workshop.c@gmail.com>
2017-03-10 10:54:54 -0300

    Segundoteste
    teste

---
+++ src/Teste/AnotherGittest/Arrumamalae.java
@@ -0,0 +1,11 @@
+package Teste.AnotherGittest;
+
+/**
+ * Created by thiago on 10/03/2017.
+ */
+public class Arrumamalae {
+
+    public static void main(String[] args) {
+        int laisEstaComNojoooo=10000;
+    }
+}
--- src/teste.java
+++ src/teste.java
@@ -3,5 +3,6 @@
  */
 public class teste {
     public static void main(String[] args) {
+
     }
 }
