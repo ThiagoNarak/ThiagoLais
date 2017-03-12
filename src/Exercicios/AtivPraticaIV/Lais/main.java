d789fcd7a4c3fe482948f24841cc02e78c37e99f
thiagoRCOliveira <thiago.workshop.c@gmail.com>
2017-03-12 17:28:13 -0300

    implementando AtivPraticaIV lais implementado main

---
+++ src/Exercicios/AtivPraticaIV/Lais/main.java
@@ -0,0 +1,10 @@
+package Exercicios.AtivPraticaIV.Lais;
+
+/**
+ * Created by laisa on 12/03/2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println(" bla bla bla");
+    }
+}
