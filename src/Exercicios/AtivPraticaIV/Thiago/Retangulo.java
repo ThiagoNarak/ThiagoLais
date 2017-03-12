6a7aba5fa68b21318a3e8d706ac7ce030fd37868
Thiago rodrigo <thiago.workshop.c@gmail.com>
2017-03-11 21:50:19 -0300

    implementando AtivPraticaIV

--- src/Exercicios/AtivPraticaIV/Thiago/Retangulo.java
+++ src/Exercicios/AtivPraticaIV/Thiago/Retangulo.java
@@ -6,17 +6,42 @@ package Exercicios.AtivPraticaIV.Thiago;
 public class Retangulo extends Poligono {
     protected Ponto[] vertices;
 
-    public static Retangulo create(Ponto ponto1,Ponto ponto2){
-        //TODO:
+    //-=============METODO
 
+    public static Retangulo criar(Ponto ponto1,Ponto ponto3){
+        double subtracaoX= ponto1.getX()-ponto3.getX();
+        double subtracaoY=ponto1.getY()-ponto3.getX();
+        if(subtracaoX==0||subtracaoY==0){
+            return null;
+        }
+    if(ponto1.getX()>ponto3.getX()){
+            if(ponto1.getY()>ponto3.getY()){
+                Ponto ponto2 = new Ponto(ponto1.getX(),ponto3.getY());
+                Ponto ponto4 = new Ponto(ponto3.getX(),ponto1.getY());
+            }else{
+                Ponto ponto2 = new Ponto(ponto3.getX(),ponto1.getY());
+                Ponto ponto4 = new Ponto(ponto1.getX(),ponto3.getY());
+            }
+    }else{
+        if(ponto3.getY()>ponto1.getY()){
+            Ponto ponto2 = new Ponto(ponto3.getX(),ponto1.getY());
+            Ponto ponto4 = new Ponto(ponto1.getX(),ponto3.getY());
 
+        }else{
+            Ponto ponto2 = new Ponto(ponto3.getX(),ponto1.getY());
+            Ponto ponto4 = new Ponto(ponto1.getX(),ponto3.getY());
 
-    return null;
+        }
+    }
+
+        //TODO: FINALIZAR IMPLEMENTAÇÃO
+
+    return null ;
     }
 
     public Retangulo(Ponto ponto1,Ponto poto2,Ponto ponto3,Ponto ponto4){
 
-    //TODO:
+    //TODO: construtor finalizar implementaçao
 
     }
 
