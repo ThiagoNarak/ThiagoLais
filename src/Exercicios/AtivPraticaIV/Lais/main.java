3bd89a31a68d414772b584d43ad9f32f805c8009
thiagoRCOliveira <thiago.workshop.c@gmail.com>
2017-03-13 10:08:44 -0300

    implementando AtivPraticaIV teste

--- src/Exercicios/AtivPraticaIV/Lais/main.java
+++ src/Exercicios/AtivPraticaIV/Lais/main.java
@@ -8,5 +8,7 @@ public class main {
         System.out.println(" bla bla bla");
 
         //comentei fiz o  codigo doidera e foi.
+
+    // TESTE LAIS GIT
     }
 }
--- src/Exercicios/AtivPraticaIV/Thiago/Mainteste2.java
+++
@@ -1,7 +0,0 @@
-package Exercicios.AtivPraticaIV.Thiago;
-
-/**
- * Created by thiago on 12/03/2017.
- */
-public class Mainteste2 {
-}
